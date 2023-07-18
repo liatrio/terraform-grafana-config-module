306576cbb75bc750169a714357fd8eea4aab8e9d
Paul Henson <paulh@liatrio.com>
2023-07-18 14:34:25 -0700

    feat: pulls api token, and then creates a folder and a dashboard within folder

--- main.tf
+++ main.tf
@@ -12,24 +12,9 @@ locals {
     grafana_url = "https://g-631c971d51.grafana-workspace.us-east-1.amazonaws.com"
 }
 
-# Fetch the output from "moduleA"
-# data "terraform_remote_state" "managed_grafana" {
-#   backend = "s3"
-#   config = {
-#     bucket = "observability-us-east-1-dev-tf-state-test"
-#     key    = "nonprod/aws-managed-services/terraform.tfstate"
-#     region = "us-east-1"
-#   }
-# }
-
-#data "aws_secretsmanager_secret" "amg_token" {
-#  name = var.grafana_api_token
-#}
-
 data "aws_secretsmanager_secret_version" "amg_token" {
   secret_id = "AMG_API_Token"
 }
-
 
 provider "grafana" {
   url  = local.grafana_url
@@ -42,16 +27,9 @@ resource "grafana_folder" "this" {
 }
 
 resource "grafana_dashboard" "this" {
+  depends_on = [grafana_folder.this]
+
   org_id = local.org_id
-  folder = "adrielp"
-  config_json = jsonencode({
-    id            = 12345,
-    uid           = "test-ds-dashboard-uid"
-    title         = "Production Overview",
-    tags          = ["templated"],
-    timezone      = "browser",
-    schemaVersion = 16,
-    version       = 0,
-    refresh       = "25s"
-  })
+  folder = grafana_folder.this.id
+  config_json = file("test2.json")
 }
