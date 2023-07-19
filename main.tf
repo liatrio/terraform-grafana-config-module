a145bb92ab82abee4fc8c652877e03e769699a4a
Paul Henson <paulh@liatrio.com>
2023-07-19 12:40:08 -0700

    feat: added functionality so it will create as many dashboards per sub folder

--- main.tf
+++ main.tf
@@ -10,6 +10,13 @@ terraform {
 locals {
     org_id = 1
     grafana_url = "https://g-631c971d51.grafana-workspace.us-east-1.amazonaws.com"
+    subfolders_and_files_set = fileset("./dashboards", "**")
+    subfolders_and_files = distinct(split(", ", join(", ", split("/", join(", ", local.subfolders_and_files_set)))))
+    subfolder_name = [for entry in local.subfolders_and_files : entry if length(regexall("[.]", entry)) == 0]
+
+    folder_map = {
+      for folder in toset(local.subfolder_name) : folder => grafana_folder.dashboard_folders[folder]
+    }
 }
 
 data "aws_secretsmanager_secret_version" "amg_token" {
@@ -21,15 +28,33 @@ provider "grafana" {
   auth = data.aws_secretsmanager_secret_version.amg_token.secret_string
 }
 
-resource "grafana_folder" "this" {
+resource "grafana_folder" "dashboard_folders" {
+  for_each = toset(local.subfolder_name)
+  
   org_id = local.org_id
-  title  = "adrielp"
+  title  = each.key
 }
 
-resource "grafana_dashboard" "this" {
-  depends_on = [grafana_folder.this]
+resource "grafana_dashboard" "prometheus_dashboards" {
+  depends_on = [grafana_folder.dashboard_folders]
 
+  for_each    = fileset("${path.module}/dashboards/prometheus", "*.json")
+
+  overwrite = true
   org_id = local.org_id
-  folder = grafana_folder.this.id
-  config_json = file("test2.json")
+  folder = local.folder_map["prometheus"].id
+
+  config_json = file("${path.module}/dashboards/prometheus/${each.key}")
+}
+
+resource "grafana_dashboard" "cloudwatch_dashboards" {
+  depends_on = [grafana_folder.dashboard_folders]
+
+  for_each    = fileset("${path.module}/dashboards/cloudwatch", "*.json")
+
+  overwrite = true
+  org_id = local.org_id
+  folder = local.folder_map["cloudwatch"].id
+
+  config_json = file("${path.module}/dashboards/cloudwatch/${each.key}")
 }
