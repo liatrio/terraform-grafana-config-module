3a903b9a3b3f719396f6de9baf7bb62b6a6bda9b
Paul Henson <paulh@liatrio.com>
2023-07-20 12:43:12 -0700

    refactor: removed two resources and replaced them with a dynamic resource that creates dashbaords for every folder and json file within

--- main.tf
+++ main.tf
@@ -10,13 +10,29 @@ terraform {
 locals {
     org_id = 1
     grafana_url = "https://g-631c971d51.grafana-workspace.us-east-1.amazonaws.com"
+    
+    # Grabs the subfolders and files within the dashboards folder.
     subfolders_and_files_set = fileset("./dashboards", "**")
-    subfolders_and_files = distinct(split(", ", join(", ", split("/", join(", ", local.subfolders_and_files_set)))))
-    subfolder_name = [for entry in local.subfolders_and_files : entry if length(regexall("[.]", entry)) == 0]
-
-    folder_map = {
-      for folder in toset(local.subfolder_name) : folder => grafana_folder.dashboard_folders[folder]
+    # Creates another list that contains just the names of the subfolders.
+    subfolder_names = distinct([for path in local.subfolders_and_files_set : dirname(path)])
+    # Pulls the actual grafana folder details including id.
+    grafana_folder_map = {
+      for folder in toset(local.subfolder_names) : folder => grafana_folder.dashboard_folders[folder]
     }
+    # Organizes the data so each folder is a key with the Json being entries in a list.
+    folder_files_map = {
+      for folder in local.subfolder_names :
+      folder => [for file in local.subfolders_and_files_set : file if dirname(file) == folder]
+    }
+    # Flattens the map so that terraform can create the resources in one pass.
+    flattened_dashboard_data = flatten([
+    for folder_name, file_paths in local.folder_files_map : [
+      for file_path in file_paths : {
+        folder_name = folder_name
+        file_path   = file_path
+      }
+    ]
+  ])
 }
 
 data "aws_secretsmanager_secret_version" "amg_token" {
@@ -29,32 +45,17 @@ provider "grafana" {
 }
 
 resource "grafana_folder" "dashboard_folders" {
-  for_each = toset(local.subfolder_name)
+  for_each = toset(local.subfolder_names)
   
   org_id = local.org_id
   title  = each.key
 }
 
-resource "grafana_dashboard" "prometheus_dashboards" {
-  depends_on = [grafana_folder.dashboard_folders]
-
-  for_each    = fileset("${path.module}/dashboards/prometheus", "*.json")
+resource "grafana_dashboard" "dynamic_dashboard" {
+  for_each = { for item in local.flattened_dashboard_data : item.file_path => item }
 
   overwrite = true
-  org_id = local.org_id
-  folder = local.folder_map["prometheus"].id
-
-  config_json = file("${path.module}/dashboards/prometheus/${each.key}")
-}
-
-resource "grafana_dashboard" "cloudwatch_dashboards" {
-  depends_on = [grafana_folder.dashboard_folders]
-
-  for_each    = fileset("${path.module}/dashboards/cloudwatch", "*.json")
-
-  overwrite = true
-  org_id = local.org_id
-  folder = local.folder_map["cloudwatch"].id
-
-  config_json = file("${path.module}/dashboards/cloudwatch/${each.key}")
+  folder  = local.grafana_folder_map[each.value["folder_name"]].id
+  config_json = file("${path.module}/dashboards/${each.key}")
+  # Other dashboard configuration options can be added here if needed
 }
