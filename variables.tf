8089d2c2ea43ede627b5aa709f41ca9f9fe1d5ef
shwaddell28 <shwaddell28@gmail.com>
2023-07-25 15:45:49 -0500

    chore: format terraform

--- variables.tf
+++ variables.tf
@@ -5,33 +5,33 @@ variable "grafana_asm_key" {
 }
 
 variable "aws_region" {
-    description = "Contains the AWS Region that Grafana is running in."
-    type = string
-    default = "us-east-1"
+  description = "Contains the AWS Region that Grafana is running in."
+  type        = string
+  default     = "us-east-1"
 }
 
 variable "data_source_map" {
-    description = "A list of maps representing datasource information. Each map defines a data source with its type, name, and url."
-    type = list(map(string))
-    default = [
-    {"data_source_type" = "cloudwatch"
-    "data_source_name" = "CloudName1"
-    "data_source_url" = "https://cloudwatch.amazonaws.com"},
-    {"data_source_type" = "prometheus"
-    "data_source_name" = "PromName1"
-    "data_source_url" = "https://prometheus.amazonaws.com"},
-    {"data_source_type" = "prometheus"
-    "data_source_name" = "PromName2"
-    "data_source_url" = "https://prometheus2.amazonaws.com"}
-    ]
+  description = "A list of maps representing datasource information. Each map defines a data source with its type, name, and url."
+  type        = list(map(string))
+  default = [
+    { "data_source_type" = "cloudwatch"
+      "data_source_name" = "CloudName1"
+    "data_source_url" = "https://cloudwatch.amazonaws.com" },
+    { "data_source_type" = "prometheus"
+      "data_source_name" = "PromName1"
+    "data_source_url" = "https://prometheus.amazonaws.com" },
+    { "data_source_type" = "prometheus"
+      "data_source_name" = "PromName2"
+    "data_source_url" = "https://prometheus2.amazonaws.com" }
+  ]
 }
 
 variable "dashboard_configs_folder" {
-    description = "The path to the dashboards folder where the configuration files are located."
-    type = string
+  description = "The path to the dashboards folder where the configuration files are located."
+  type        = string
 }
 
 variable "grafana_url" {
-    description = "The url to your Grafana instance."
-    type = string
+  description = "The url to your Grafana instance."
+  type        = string
 }
